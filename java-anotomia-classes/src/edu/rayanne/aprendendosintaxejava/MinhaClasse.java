fc9aa2b180aec62c84d1cf37164a609a7ebb9777
Rayannecristinar <rayanne.cristinar@gmail.com>
2022-08-26 15:30:58 -0300

    ultimos arquivos do java basico

--- java-anotomia-classes/src/edu/rayanne/aprendendosintaxejava/MinhaClasse.java
+++ java-anotomia-classes/src/edu/rayanne/aprendendosintaxejava/MinhaClasse.java
@@ -2,7 +2,7 @@ package edu.rayanne.aprendendosintaxejava;
 public class MinhaClasse {
     
 public static void main (String [] args ){
-    System.out.print ("Ola Rayanne");
+    System.out.print ("Ola Rayanne, foi executado pelo Terminal");
 }
 
 }
